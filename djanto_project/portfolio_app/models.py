45074e8e8b9fcd905fbb7d4611164f98d99aef7c
Kenyou Teoh <kenyouteoh@gmail.com>
2023-10-02 19:31:35 -0600

    a

--- djanto_project/portfolio_app/models.py
+++ djanto_project/portfolio_app/models.py
@@ -1,7 +1,23 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
+class Portfolio(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    isActive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Project(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, default = None)
 
 class Student(models.Model):
     # List of choices for major value in database, human readable name
@@ -17,4 +33,27 @@ class Student(models.Model):
 
     name = models.CharField(max_length=200)
     email = models.CharField("UCCS Email", max_length=200)
-    major = models.CharField(max_length=200, choices=MAJOR, blank=True)
+    major = models.CharField(max_length=200, choices=MAJOR)
+
+    # Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+    # Returns the URL to access a particular instance of MyModelName.
+    # if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
+
+    portfolio = models.OneToOneField(Portfolio, on_delete=models.CASCADE, unique=True)
+
+class ProjectsInPortfolio(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE) 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.portfolio.title} - {self.project.title}"
+
+class Meta:
+    #ensures that each project is associated with only one portfolio
+    unique_together = ('portfolio', 'project')
